9f06f746281e033f85c5d42fd90de9000d088a76
jose <jose.alvarez@consulting-for.edenred.com>
2023-02-27 13:09:14 -0600

    add logincontroller

---
+++ DemoSwift/DemoSwift/Controllers/LoginController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginController.swift
+//  DemoSwift
+//
+//  Created by Jose on 27/02/23.
+//
+
+import Foundation
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}

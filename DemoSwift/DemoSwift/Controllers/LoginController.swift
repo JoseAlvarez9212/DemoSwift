73c1a237da8b1e5429ce4c4015615f619be215b4
jose <jose.alvarez@consulting-for.edenred.com>
2023-03-09 12:13:53 -0600

    AddButtonView

--- DemoSwift/DemoSwift/Controllers/LoginController.swift
+++ DemoSwift/DemoSwift/Controllers/LoginController.swift
@@ -8,11 +8,30 @@
 import Foundation
 import UIKit
 
-class LoginController: UIViewController {
+final class LoginController: UIViewController {
+    
+    private let userTextField: UIView = DemoButton(frame: .zero)
+    
+    override func loadView() {
+        super.loadView()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        userTextFielConfig()
+    }
+    
+    private func userTextFielConfig()
+    {
+        view.addSubview(userTextField)
+        userTextField.translatesAutoresizingMaskIntoConstraints = false
+        userTextField.layout{
+            $0.top == view.safeAreaLayoutGuide.topAnchor + 10
+            $0.trailing == view.trailingAnchor - 10
+            $0.leading == view.leadingAnchor + 10
+        }
+        
     }
 
 
